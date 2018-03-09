538abc8aa9c69f766ca7611ff8a120d5e49379ad
minatojoho <minato.joho+github@gmail.com>
2018-03-09 09:53:58 +0900

    Sorcery による基本的なアカウントロジックの実装

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,9 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  protected
+
+  def not_authenticated
+    redirect_to welcome_path
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
+  get '/welcome', to: "sessions#new", as: :welcome
+  post '/login', to: "sessions#create", as: :login
+  delete '/logout', to: "sessions#destroy", as: :logout
+  resources :users
+  root to: 'home#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
