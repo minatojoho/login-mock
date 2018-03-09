3fe52fb26dd697a2d119533b13037f9f7ef0f4e7
minatojoho <minato.joho+github@gmail.com>
2018-03-09 16:55:47 +0900

    CloudFront に対応させる Rack Middleware を削除

--- config/application.rb
+++ config/application.rb
@@ -11,9 +11,5 @@ module LoginMock
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
-
-    # For AWS CloudFront
-    require './lib/cloudfront_header.rb'
-    config.middleware.use CloudfrontHeader
   end
 end
