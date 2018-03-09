0dbc5fc04fd1ad573ba3ae21f280e2c7fff6ba95
minatojoho <minato.joho+github@gmail.com>
2018-03-09 17:06:52 +0900

    base_url と origin を参照するためのメソッドを追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,14 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  before_action :dump_env
 
   protected
+
+  def dump_env
+    p "====="
+    p request.base_url
+    p request.headers['origin']
+  end
 
   def not_authenticated
     redirect_to welcome_path
